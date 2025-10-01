582bff49c9daac2f756b1ced00a89d53d9cce9d0
Kseniya1111 <abbasovakm@gmail.com>
2025-10-01 12:01:40 +0200

    path has been changed

--- src/com/example/media/app/MediaProcessor.java
+++ src/com/example/media/app/MediaProcessor.java
@@ -12,8 +12,8 @@ public class MediaProcessor {
 
     private static final String TRACKS_INPUT = "data/tracks.txt";
     private static final String VIDEOS_INPUT = "data/videos.txt";
-    private static final String TRACKS_OUTPUT = "tracks_output.txt";
-    private static final String VIDEOS_OUTPUT = "videos_output.txt";
+    private static final String TRACKS_OUTPUT = "out/tracks_output.txt";
+    private static final String VIDEOS_OUTPUT = "out/videos_output.txt";
 
     public static void main(String[] args) throws IOException {
 
